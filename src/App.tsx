b57cc3549f1ddaa84835004f5456d6d349bdb090
Hue <tohuem@gmail.com>
2020-05-16 21:21:52 -0500

    Updated home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import './Common.scss';
 import { render } from '@testing-library/react';
 import Nav from './views/layout/Nav';
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -5,10 +5,15 @@ const teatime = require('../../assets/img/teatime.png');
 export default class Home extends React.Component {
     render(){
         return (
-            <div className="cover-container">
-                <img src={teatime} className="cover"/>
-                <button className="shop-now">Shop Now</button>
-            </div>
+            <>
+                <div className="cover-container">
+                    <img src={teatime} className="cover"/>
+                    <button className="shop-now">Shop Now</button>
+                </div>
+                <div className="home-content">
+                    <h4 className="left">Featured products</h4>
+                </div>
+            </>
         )
     }
 }
